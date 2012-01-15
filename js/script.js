0060c90d467a9ba17c7cf119861fd806241d243f
grayghostvisuals <grayghost@grayghostvisuals.com>
2012-01-14 20:51:27 -0500

    js folder and libraries initial push

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	
+	//===================================================================================================================== Progressive Enhancements
+	
+	function elementSupportsAttribute(element,attribute){
+		var test = document.createElement(element);
+		if( attribute in test){
+			return true;
+			}else{
+				return false;
+				}
+		};//end function elementSupportsAttribute
+	
+	
+	//=====================================================================================================================jQuery Form Validator w/Ajax
+	
+		
+	//==== jQuery validator default settings
+	jQuery.validator.setDefaults({
+		debug: false,
+		//handler for form submissions
+		submitHandler: function(form){ 
+		
+			//==== ajax options object
+			var options = {
+				clearForm: true,
+				resetForm: true,
+				clearFields: true,
+				success : function(){ 
+					alert('Thanks for your submission friend! No need to hit the submit button again. We have your message and we will be in touch'); 
+					}//end success function
+				};//end options for ajax form submission
+				
+			//==== submit the form via ajax
+			$(form).ajaxSubmit(options); 
+			
+			}//end submitHandler
+			
+	});
+		
+	$().validate({});
+	
+});//end document.ready()
