a5bd3816d7deb7c005783487f5f94546a1352049
grayghostvisuals <grayghost@grayghostvisuals.com>
2012-01-14 20:50:34 -0500

    fixed jquery ref in the footer

--- footer.php
+++ footer.php
@@ -76,7 +76,7 @@
   <!-- JavaScript at the bottom for fast page loading -->
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.js?v=1.6.4.min"><\/script>')</script>
+  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.min.js?v=1.6.4"><\/script>')</script>
   
   <!-- scripts concatenated and minified via ant build script-->
   <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js?v=0.0.0"></script>
