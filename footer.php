b8194fbeccea42b036f5040a8c735420bb5ed01e
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-02-27 18:40:36 -0500

    bootstrap script inclusion

--- footer.php
+++ footer.php
@@ -69,8 +69,11 @@
 
   <!-- JavaScript at the bottom for fast page loading -->
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery.min.js"><\/script>')</script>
+  
+  <!-- bootstrap.min.js -->
+  <script src="<?php echo get_template_directory_uri(); ?>/js/libs/bootstrap.js"></script>
   
   <!-- global scripts and plugins -->
   <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
