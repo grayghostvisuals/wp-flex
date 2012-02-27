82fc67dd5c0ba49b68d99fe890911f5b1251cc7c
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-02-27 11:56:52 -0500

    comments init commit

---
+++ comments.php
@@ -0,0 +1,81 @@
+<?php
+if( !empty( $_SERVER[ 'SCRIPT_FILENAME' ] ) && 'comments.php' == basename( $_SERVER[ 'SCRIPT_FILENAME' ] ) ) :
+die('please do not load this page directly mate');
+?>
+
+<section class="comments">
+  <?php if ( post_password_required() ) : ?>
+  <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.' ); ?></p>
+  <?php return; ?>
+  <?php endif; ?>
+  
+  <?php // begin comment count ?>
+  <?php if ( have_comments() ) : ?>
+  
+  <h3 id="comments-title"><?php printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number() ), number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' ); ?></h3>
+  
+  <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+  
+  <div class="comment-pagination"><?php previous_comments_link( _e( '<span class="meta-nav">&larr;</span>' ) ); ?><?php next_comments_link( _e( '<span class="meta-nav">&rarr;</span>' ) ); ?></div>
+  <!-- .comment-pagination -->
+  
+  <?php endif; //end check for comment navigation ?>
+  
+  <ol class="commentlist">
+    <?php foreach( $comments as $comment )?>
+    <?php	$ridiculousReview_comment_array = array(
+    					'walker' 			=> null,
+    					'max_depth' 		=> '',
+    					'style' 			=> 'ol', 
+    					'callback' 			=> null,
+    					'end-callback' 		=> null,
+    					'type' 				=> 'all',
+						'reply_text'		=> 'shout back',
+    					'page' 				=> '',
+    					'per_page' 			=> '',
+    					'avatar_size' 		=> 64,
+    					'reverse_top_level' => true,
+    					'reverse_children' 	=> true 
+    					); 
+
+    		//see developer docs http://codex.wordpress.org/Function_Reference/wp_list_comments
+
+		?>
+        
+    <?php if ($comment->comment_approved == '0') : ?>
+    <p class="moderating"><em><?php _e('Your rant, suggestion, or comment is awaiting moderation from the admin. Please be patient') ?></em></p>
+	<?php endif; ?>
+    
+    <?php wp_list_comments($ridiculousReview_comment_array) ?>
+  
+  </ol>
+  
+  <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+  
+  <div class="comment-pagination">
+    <?php previous_comments_link( _e( '<span class="meta-nav">&larr;</span>' ) ); ?>
+    <?php next_comments_link( _e( '<span class="meta-nav">&rarr;</span>' ) ); ?>
+  </div>
+  <!-- .comment-pagination -->
+  
+  <?php endif; // check for comment navigation ?>
+  
+  <?php else : 
+	//If there are no comments and comments are closed,
+	//let's leave a little note, shall we?
+
+	if ( !comments_open() ) :
+   ?>
+    
+  <p class="nocomments"><img src="<?php echo get_template_directory_uri(); ?>/img/bullhorn64.png" alt="" />
+    <span class="speech-arrow"></span><b><?php _e( 'comments are closed. you\'re too late unfortunately mate' ); ?></b>
+  </p>
+  
+  <?php endif; //endif !comments_open ?>
+  
+  <?php endif; //endif have_comments ?>
+  
+  <?php comment_form(); ?>
+
+</section>
+<!-- end /section.comments -->
