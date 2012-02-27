4d28aea038299472eb2588d061927654c0025d22
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-02-27 10:21:43 -0500

    comments removed and classes once again taken out

--- index.php
+++ index.php
@@ -9,44 +9,35 @@
       <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
         <header>
           <?php if ( has_post_thumbnail() ) : ?>
-          <figure class="post-thumbnail">
+          <figure>
             <?php the_post_thumbnail(); ?>
           </figure>
-          
 		  <?php endif; //end if has_post_thumbnail ?>
           
-          <h1 class="post-title"><span><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?> blog post entry"><?php the_title(); ?></a></span></h1>
-          
-          <small class="post-copyright"> <span class="post-author"></span><time class="post-datetime" datetime="%3$s" pubdate><?php the_time( get_option( 'date_format' ) ); ?></time></small> 
+          <h1><span><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?> blog post entry"><?php the_title(); ?></a></span></h1>
+          <small><span>posted by: <?php the_author(); ?></span> on <time datetime="%3$s" pubdate><?php the_time( get_option( 'date_format' ) ); ?></time></small> 
           </header>
           
-        <section class="post-txt">
+        <section>
           <?php the_content( '<span>read more</span>' ); ?>
         </section>
-        <!-- end /div.post-txt -->
         
         <!-- post footer -->
-        <footer class="post-footer">
-          <div class="post-comments"><span class="comments-link"><a href="<?php comments_link(); ?>"><?php comments_number( '0', '1', '%' ); ?>Comments</a></span></div>
-          <!-- end /div.post-comments -->
+        <foote>
+          <div><span><a href="<?php comments_link(); ?>"><?php comments_number( '0', '1', '%' ); ?>Comments</a></span></div>
           
-          <div class="post-meta"> 
-          	<span class="post-tags"><?php the_tags('Tags', ','); ?></span> 
-          	<span class="post-categories">Posted in <?php the_category(',') ?></span>
-          </div>
-           <!-- end /div.post-meta -->
+          <div><span>Tagged: <?php the_tags('Tags', ','); ?></span> <span>Posted in: <?php the_category(',') ?></span></div>
         </footer>
       </article>
-      <!-- end /article the_post -->
+      
       <?php endwhile; //end while have_posts loop ?>
       
 	  <?php else : //if no posts were found do this ?>
       	<p><?php echo ( 'Holy smokes! This is totally crazy. No posts match anything even remotely close to that in our database. Sorry Mon Frere, try again' ); ?></p>
       <?php endif; //end if have_posts condition ?>
     </section>
-    <!-- end /section#content -->
     
-    <div id="index-pagination" class="pagination">
+    <div>
       <?php 
 		global $wp_query;
 		$big = 999999999;
@@ -66,10 +57,8 @@
 		'add_fragment' => '' ) );
 		?>
     </div>
-    <!-- end /div#index-pagination-->
     
   </div>
-  <!-- end /div#main-->
 
 </div>
 <!--! end /div#container -->
