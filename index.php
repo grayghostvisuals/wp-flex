5193924a625750c5735c78ae6f45dfa40432b737
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-02-27 08:42:18 -0500

    index cleaning and removal of useless IDs and classes

--- index.php
+++ index.php
@@ -1,75 +1,47 @@
 <?php get_header(); ?>
+<div id="container">
 
-<div id="container" class="clearfix">
+  <div role="main">
 
-  <div id="main" role="main">
-
-    <section id="content" class="clearfix">
-
+    <section>
       <!-- begin post loop -->
       <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-      
       <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-      
-        <header class="post-head">
-      
+        <header>
           <?php if ( has_post_thumbnail() ) : ?>
-          
           <figure class="post-thumbnail">
-          
             <?php the_post_thumbnail(); ?>
-          
           </figure>
           
 		  <?php endif; //end if has_post_thumbnail ?>
           
-          <h1 class="post-title"> 
-          <span> 
-          <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?> blog post entry">
-            <?php the_title(); ?>
-          </a> 
-          </span> 
-          </h1>
+          <h1 class="post-title"><span><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?> blog post entry"><?php the_title(); ?></a></span></h1>
           
-          <small class="post-copyright"> <span class="post-author"></span>
-          <time class="post-datetime" datetime="%3$s" pubdate>
-            <?php the_time( get_option( 'date_format' ) ); ?>
-          </time>
-          </small> 
+          <small class="post-copyright"> <span class="post-author"></span><time class="post-datetime" datetime="%3$s" pubdate><?php the_time( get_option( 'date_format' ) ); ?></time></small> 
           </header>
           
         <section class="post-txt">
-          <?php the_content( '<span class="read-more button-styling">read more</span>' ); ?>
+          <?php the_content( '<span>read more</span>' ); ?>
         </section>
         <!-- end /div.post-txt -->
         
         <!-- post footer -->
         <footer class="post-footer">
-        
-          <div class="post-comments"> 
-          
-          <span class="comments-link"><a href="<?php comments_link(); ?>"><?php comments_number( '0', '1', '%' ); ?>Comments</a></span>
-          
-          </div>
+          <div class="post-comments"><span class="comments-link"><a href="<?php comments_link(); ?>"><?php comments_number( '0', '1', '%' ); ?>Comments</a></span></div>
           <!-- end /div.post-comments -->
           
           <div class="post-meta"> 
-          
-          <span class="post-tags"><?php the_tags('Tags', ','); ?></span> 
-          
-          <span class="post-categories">Posted in <?php the_category(',') ?></span> 
-          
+          	<span class="post-tags"><?php the_tags('Tags', ','); ?></span> 
+          	<span class="post-categories">Posted in <?php the_category(',') ?></span>
           </div>
            <!-- end /div.post-meta -->
-            
         </footer>
-        
       </article>
       <!-- end /article the_post -->
       <?php endwhile; //end while have_posts loop ?>
       
 	  <?php else : //if no posts were found do this ?>
-      <p><?php echo ( 'Holy smokes! This is totally crazy. No posts match anything even remotely close to that in our database. Sorry Mon Frere, try again' ); ?></p>
+      	<p><?php echo ( 'Holy smokes! This is totally crazy. No posts match anything even remotely close to that in our database. Sorry Mon Frere, try again' ); ?></p>
       <?php endif; //end if have_posts condition ?>
     </section>
     <!-- end /section#content -->
@@ -101,5 +73,4 @@
 
 </div>
 <!--! end /div#container -->
-<!-- google_ad_section_end -->
 <?php get_footer(); ?>
