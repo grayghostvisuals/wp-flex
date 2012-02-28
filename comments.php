cdc2787f856428c03956c468e1da66408485b59c
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-02-28 18:00:50 -0500

    added base styling hooks for comments and thread children for that tricky comment nesting stuff wordpress does

--- comments.php
+++ comments.php
@@ -23,32 +23,32 @@ die('please do not load this page directly mate');
   <?php endif; //end check for comment navigation ?>
   
   <ol class="commentlist">
-    <?php foreach( $comments as $comment )?>
+    <?php foreach( $comments as $comment ) ?>
     <?php	
-		//comment array
-		//see developer docs http://codex.wordpress.org/Function_Reference/wp_list_comments
-		$theme_comment_array = array(
-									  'walker' 				=> null,
-									  'max_depth' 			=> '',
-									  'style' 				=> 'ol', 
-									  'callback' 			=> null,
-									  'end-callback' 		=> null,
-									  'type' 				=> 'all',
-									  'reply_text'			=> 'shout back',
-									  'page' 				=> '',
-									  'per_page' 			=> '',
-									  'avatar_size' 		=> 32,
-									  'reverse_top_level' 	=> true,
-									  'reverse_children' 	=> true 
-									); 
+	//comment array
+	//see developer docs http://codex.wordpress.org/Function_Reference/wp_list_comments
+	$themename_comment_array = array(
+								  'walker' 				=> null,
+								  'max_depth' 			=> '',
+								  'style' 				=> 'ol', 
+								  'callback' 			=> null,
+								  'end-callback' 		=> null,
+								  'type' 				=> 'all',
+								  'reply_text'			=> 'shout back',
+								  'page' 				=> '',
+								  'per_page' 			=> '',
+								  'avatar_size' 		=> 32,
+								  'reverse_top_level' 	=> true,
+								  'reverse_children' 	=> true 
+								); 
 
-		?>
+	?>
         
-    <?php if ( $comment->comment_approved == '0' ) : ?>
+    <?php if ( $comment -> comment_approved == '0' ) : ?>
     <p class="moderating"><em><?php _e('Your rant, suggestion, or comment is awaiting moderation from our head cheese. Please be patient') ?></em></p>
     <?php endif; ?>
     
-    <?php wp_list_comments( $theme_comment_array ) ?>
+    <?php wp_list_comments( $themename_comment_array ) ?>
   </ol>
   
   <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
