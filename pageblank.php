d139b4f6f70118da285870aec345e48ecd0feaed
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-02-28 21:26:26 -0500

    blank page template for themes if desired with comments setup

---
+++ pageblank.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+ * Required to display in select menu list
+ * on posts pages from dashboard
+ * Template Name: Blank
+ */
+?>
+
+<?php get_header(); ?>
+	
+<div id="container">
+<div id="main" role="main">
+	
+<div class="clearfix">
+    <?php the_content(); ?>
+</div>
+
+</div>
+<!-- end /div#main-->
+</div> 
+<!--! end /div#container -->
+
+<?php get_footer(); ?>
