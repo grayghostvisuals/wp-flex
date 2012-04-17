871573d3408926302fe751ac5062d2b6c7d90fe3
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-04-17 14:28:20 -0400

    cleaned up comments pagination and gave better semantics and styling hooks

--- comments.php
+++ comments.php
@@ -1,29 +1,32 @@
 <?php
 if( ! empty( $_SERVER[ 'SCRIPT_FILENAME' ] ) && 'comments.php' == basename( $_SERVER[ 'SCRIPT_FILENAME' ] ) )
-die('please do not load this page directly mate');
+die('please do not load this page directly kind sir');
 ?>
 
 <section class="comments">
 
   <?php if ( post_password_required() ) : ?>
-  <p class="nopassword"><?php echo( 'This post is password protected. Enter the password to view any comments.' ); ?></p>
+  	<p class="nopassword"><?php echo( 'This post is password protected. Enter the password to view any comments.' ); ?></p>
   <?php return; ?>
   <?php endif; ?>
   
   <!-- begin comment count -->
   <?php if ( have_comments() ) : ?>
-  <h3 id="comments-title"><?php printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number() ), number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' ); ?></h3>
+  	<h3 id="comments-title"><?php printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number() ), number_format_i18n( get_comments_number() ), '<em>' . get_the_title() . '</em>' ); ?></h3>
   
   <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // check if there comments to navigate through ?>
-  <div class="comment-pagination">
-    <?php previous_comments_link( '<span>&larr;</span>' ); ?>
-    <?php next_comments_link( '<span>&rarr;</span>' ); ?>
-  </div>
-  <!-- .comment-pagination -->
+  	
+    <div class="comment-pagination">
+		<span class="prev-comments-link"><?php previous_comments_link( '<span>&larr; older comments</span>' ); ?></span>
+    	<span class="nxt-comments-link"><?php next_comments_link( '<span>newer comments &rarr;</span>' ); ?></span>
+  	</div>
+  	<!-- .comment-pagination -->
+  
   <?php endif; //end check for comment navigation ?>
   
   <ol class="commentlist">
     <?php foreach( $comments as $comment ) ?>
+
     <?php	
 	//comment array
 	//see developer docs http://codex.wordpress.org/Function_Reference/wp_list_comments
@@ -39,35 +42,30 @@ die('please do not load this page directly mate');
 								  'per_page' 			=> '',
 								  'avatar_size' 		=> 32,
 								  'reverse_top_level' 	=> true,
-								  'reverse_children' 	=> true 
-								); 
+								  'reverse_children' 	=> true,
+								); ?>
 
-	?>
-        
     <?php if ( $comment -> comment_approved == '0' ) : ?>
-    <p class="moderating"><em><?php echo('Your rant, suggestion, or comment is awaiting moderation from our head cheese. Please be patient') ?></em></p>
-    <?php endif; ?>
-    
-    <?php wp_list_comments( $themename_comment_array ) ?>
+    	<p class="moderating"><em><?php echo('Your rant, suggestion, or comment is awaiting moderation from our head cheese. Please be patient') ?></em></p>
+	<?php endif; ?>
+    <?php wp_list_comments( $themename_comment_array ); ?>
   </ol>
-  
+
   <?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
   
   <div class="comment-pagination">
-    <?php previous_comments_link( '<span>&larr;</span>' ); ?>
-    <?php next_comments_link( '<span>&rarr;</span>' ); ?>
+	<span class="prev-comments-link"><?php previous_comments_link( '<span>&larr; older comments</span>' ); ?></span>
+    <span class="nxt-comments-link"><?php next_comments_link( '<span>newer comments &rarr;</span>' ); ?></span>
   </div>
   <!-- .comment-pagination -->
   
   <?php endif; // check for comment navigation ?>
-  <?php else : 
+  <?php 
+  	else : 
 	//If there are no comments and comments are closed,
 	//let's leave a little note, shall we?
-
-	if ( ! comments_open() ) :
-   ?>
+	if ( ! comments_open() ) : ?>
   <p class="nocomments"><?php echo( 'comments are closed. you\'re too late unfortunately mate' ); ?></p>
-  
   <?php endif; //endif !comments_open ?>
   <?php endif; //endif have_comments ?>
   
