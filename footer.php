a3445948a5162ab27bc1e3bdd8f43f4bd5320cf4
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-05-23 22:53:18 -0400

    formatted and cleansed the html and php to be more readable and easier to maintain

--- footer.php
+++ footer.php
@@ -1,79 +1,64 @@
  <footer role="contentinfo">
     
-    <section>
-      <?php if ( function_exists('dynamic_sidebar') ) : ?>
-      <?php dynamic_sidebar('footer widget') ?>
+  <section>
+    <?php if ( function_exists( 'dynamic_sidebar' ) ) : ?>
+    <?php dynamic_sidebar('footer widget') ?>
+    <?php endif; ?>
+  </section>
+  
+  <!-- vCard -->
+  <?php $options = get_option( 'wpflex_theme_options' ); ?>
+  <?php if( $options || empty($options) ): ?>
+  
+  <!-- fn org -->
+  <section>
+    <ul class="vcard">
+      <!-- fn org -->
+      <?php if( ! empty( $options['fnorg'] ) ): ?>
+      <li class="fn org"><small><?php echo ( $options['fnorg'] ); ?>,</small></li>
+      
+      <!-- fn org fallback -->
+      <?php else : ?>
+      <li><small>My Super Awesome Company</small></li>
       <?php endif; ?>
-    </section>
-  
-    <!--xxxxxxxxxxxxxxxxxx custom footer Vcard xxxxxxxxxxxxxxxxxx-->
-    <?php $options = get_option( 'wpflex_theme_options' ); ?>
-    <?php if( $options || empty($options) ): ?>
-  
-    <!-- vcard fn org -->
-    <section>
-      <ul class="vcard">
-	<!-- fn org -->
-        <?php if( ! empty( $options['fnorg'] ) ): ?>
-        <li class="fn org">
-		<small><?php echo ( $options['fnorg'] ); ?>,</small>
-	</li>
-        <!-- fn org fallback -->
-	<?php else : ?>
-        <li>
-		<small>My Super Awesome Company</small>
-	</li>
-        <?php endif; ?>
         
-        <!-- vcard adr -->
-        <?php if( $options['street'] && $options['locality'] && $options['region'] && $options['postal'] ): ?>
-        <li class="adr">
-		<small>
-			<span class="street-address"><?php echo ( $options['street'] ); ?></span>, <span class="locality"><?php echo ( $options['locality'] ); ?></span>, 
-			<span class="region"><abbr title="<?php echo ( $options['region'] ); ?>"><?php echo ( $options['region'] ); ?></abbr></span>
-			<span class="postal-code"><?php echo ( $options['postal'] ); ?></span>
-		</small>
-	</li>
-        <!-- vcard adr fallback -->
-	<?php else : ?>
-        <li class="adr">
-		<small>555 Fake Street, My Awesome City Name, NY 55555</small>
-	</li>
-        <?php endif; ?>
+      <!-- adr -->
+      <?php if( $options['street'] && $options['locality'] && $options['region'] && $options['postal'] ): ?>
+      <li class="adr">
+        <small>
+          <span class="street-address"><?php echo ( $options['street'] ); ?></span>, <span class="locality"><?php echo ( $options['locality'] ); ?></span>, 
+          <span class="region"><abbr title="<?php echo ( $options['region'] ); ?>"><?php echo ( $options['region'] ); ?></abbr></span>
+          <span class="postal-code"><?php echo ( $options['postal'] ); ?></span>
+		    </small>
+	    </li>
+      
+      <!-- adr fallback -->
+      <?php else : ?>
+        <li class="adr"><small>555 Fake Street, My Awesome City Name, NY 55555</small></li>
+      <?php endif; ?>
         
-        <!-- vcard tel -->
-        <?php if( $options['teltype'] && $options['telvalue'] ): ?>
-        <li class="tel">
-	<small>
-		<span class="type"><?php echo ( $options['teltype'] ); ?></span>: <span class="value"><?php echo ( $options['telvalue'] ); ?></span></small></li>
+      <!-- tel -->
+      <?php if( $options['teltype'] && $options['telvalue'] ): ?>
+      <li class="tel">
+        <small><span class="type"><?php echo ( $options['teltype'] ); ?></span>: <span class="value"><?php echo ( $options['telvalue'] ); ?></span></small></li>
+        
+        <!-- tel fallback -->
         <?php else : ?>
-        <li class="tel">
-		<small>Office: 555&ndash;555-5555</small>
-	</li>
+        <li class="tel"><small>Office: 555&ndash;555-5555</small></li>
         <?php endif; ?>
-        
+          
         <!-- url fn org -->
         <?php if( $options['url'] ): ?>
-        <li class="url fn org">
-		<small><a rel="bookmark" href="http://<?php echo ( $options['url'] ); ?>" title="visit us online"><?php echo ( $options['url'] ); ?></a></small>
-	</li>
+        <li class="url fn org"><small><a rel="bookmark" href="http://<?php echo ( $options['url'] ); ?>" title="visit us online"><?php echo ( $options['url'] ); ?></a></small></li>
+          
         <!-- url fn org fallback -->
-	<?php else : ?>
-        <li class="url fn org">
-		<small>
-			<a rel="bookmark" href="http://www.grayghostvisuals.com" title="visit the designer and coder online">http://www.grayghostvisuals.com</a>
-		</small> 
-	</li>
+        <?php else : ?>
+        <li class="url fn org"><small><a rel="bookmark" href="http://www.grayghostvisuals.com" title="visit the designer and coder online">http://www.grayghostvisuals.com</a></small></li>
         <?php endif; ?>
         
-        <!-- vcard add to address book -->
-        <li class="add-vcard">
-		<small>
-			<a href="http://h2vx.com/vcf/<?php echo ( $options['addrbook'] ); ?>" rel="vcard">&bull; Add Us To Your Address Book</a>
-		</small>
-	</li>
+        <!-- add to address book -->
+        <li class="add-vcard"><small><a href="http://h2vx.com/vcf/<?php echo ( $options['addrbook'] ); ?>" rel="vcard">&bull; Add Us To Your Address Book</a></small></li>
       </ul>
-    
     </section>
     <?php endif; ?>
     
@@ -83,8 +68,8 @@
       <?php if( $options[ 'facebookurl' ] ) : ?>
       <article id="footer-fb">
         <div class="facebook">
-		<a href="<?php echo ( $options[ 'facebookurl' ] ); ?>" rel="external"><?php echo ( $options[ 'facebookurl' ] ); ?></a>
-	</div>
+		      <a href="<?php echo ( $options[ 'facebookurl' ] ); ?>" rel="external"><?php echo ( $options[ 'facebookurl' ] ); ?></a>
+        </div>
       </article>
       <?php endif; ?>
 
@@ -93,8 +78,8 @@
       <?php if( $options[ 'twitterurl' ] ) : ?>
       <article id="footer-twitter">
         <div class="twitter">
-		<a href="https://twitter.com/<?php echo ( $options[ 'twitterurl' ] ); ?>" rel="external">follow<?php echo( $options[ 'twitterurl' ] ); ?></a>
-	</div>
+		      <a href="https://twitter.com/<?php echo ( $options[ 'twitterurl' ] ); ?>" rel="external">follow<?php echo( $options[ 'twitterurl' ] ); ?></a>
+        </div>
       </article>
       <?php endif; ?>
       
@@ -102,9 +87,7 @@
       <section id="copyright">
       	<small>Created with the WP&ndash;Flex boilerplate foundation by <a href="" rel="external">http://grayghostvisuals.com</a></small>
       </section>
-     
     </section>
- 
   </footer>
 
   <!-- JavaScript at the bottom for fast page loading -->
