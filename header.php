d83cb13418227869e6a39cf208b91e19d1f01ed8
Gray Ghost Visuals <grayghost@grayghostvisuals.com>
2012-07-13 18:58:17 -0400

    fixed title tag for head section where words did not separate correctly. Also some general house cleaning and white space removal

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
+<!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <head>
 
 <!-- character encoding utf-8 -->
@@ -13,35 +13,39 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 
 <!-- title -->
-<title>
-<?php 
-if( function_exists( 'is_tag' ) && is_tag() ) : single_tag_title( 'Tag Archive for &quot;' ); echo '&quot; - '; 
+<title><?php
+// if tag
+if( function_exists( 'is_tag' ) && is_tag() ) :
+  single_tag_title( 'Tag Archive for &quot;' ); echo '&quot; - ';
 
+// if archives
 elseif( is_archive() ) :
-esc_attr( wp_title( '' ) ); echo 'Archive -';
+  esc_attr( wp_title( '' ) ); echo ' '; echo 'Archive -';
 
+// if search
 elseif( is_search() ) :
-echo 'Search for &quot;' . wp_specialchars( $s ) . '&quot; -';
+  echo 'Search for &quot;' . wp_specialchars( $s ) . '&quot; -';
 
+// if !404 and single or page
 elseif( !( is_404() ) && ( is_single() ) || ( is_page() ) ) :
-esc_attr( wp_title( '' ) ); echo '-';
+  esc_attr( wp_title( '' ) ); echo '-';
 
+// if 404
 elseif( is_404() ) :
-echo 'Not Found -';
+  echo 'Not Found -';
 endif;
 
+//if home
 if( is_home() ):
-esc_attr( bloginfo( 'name' ) ); esc_attr( bloginfo( 'description' ) ); esc_attr( wp_title() );
-
+  esc_attr( bloginfo( 'name' ) ); echo ' '; esc_attr( bloginfo( 'description' ) ); echo ' '; esc_attr( wp_title() );
 else :
-esc_attr( bloginfo( 'name' ) );
+  esc_attr( bloginfo( 'name' ) );
 endif;
 
+// if pages is greater than 1
 if( $paged > 1 ) :
-echo '-page' . $paged;
-endif;
-?>
-</title>
+  echo '-page' . $paged;
+endif; ?></title>
 
 <!-- search engine robots meta instructions -->
 <?php if ( is_search() || is_404() ) : ?>
@@ -53,18 +57,14 @@ endif;
 <!-- index search meta data -->
 <?php if ( is_home() ) : ?>
 <meta name="description" content="<?php esc_attr( bloginfo( 'name' ) ); esc_attr( bloginfo( 'description' ) ); ?>">
-
 <!-- single page meta tag description -->
 <?php elseif ( is_single() ) : ?>
 <meta name="description" content="<?php esc_attr( wp_title() ) ?>">
-
 <!-- archive pages meta tag description -->
 <?php elseif ( is_archive() ) : ?>
 <meta name="description" content="">
-
 <?php elseif ( is_search() ) : ?>
 <meta name="" content="<?php wp_specialchars( $s ) ?>">
-
 <!-- fallback meta tag description -->
 <?php else : ?>
 <meta name="description" content="<?php esc_attr( bloginfo( 'name' ) ); esc_attr( bloginfo( 'description' ) ) ?>">
@@ -90,7 +90,7 @@ endif;
 <!-- RSS Feed -->
 <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo( 'rss2_url' ); ?>" />
 
-<!-- All JavaScript at the bottom, except this Modernizr. Modernizr enables HTML5 elements & feature detects; 
+<!-- All JavaScript at the bottom, except this Modernizr. Modernizr enables HTML5 elements & feature detects;
          for optimal performance, create your own custom Modernizr build: www.modernizr.com/download/ -->
 <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/modernizr.js"></script>
 
@@ -131,34 +131,35 @@ endif;
   <h2><?php echo esc_attr( bloginfo( 'description' ) ); ?></h2>
   <?php endif; ?>
   <!-- end header custom image -->
- 
+
   <!-- http://codex.wordpress.org/Function_Reference/wp_nav_menu -->
   <nav role="navigation">
     <ol>
-      <?php 
-	//wp_list_pages arguments as an array
-	$nav_wpflex = array(
-				'depth'        	=> 2,
-				'show_date'    	=> '',
-				'date_format'  	=> get_option( 'date_format' ),
-				'child_of'     	=> 0,
-				'exclude'      	=> '',
-				'include'      	=> '',
-				'title_li'     	=> '',
-				'echo'         	=> 1,
-				'authors'      	=> '',
-				'sort_column'  	=> 'menu_order',
-				'link_before'  	=> '',
-				'link_after'   	=> '',
-				'walker' 	=> '' 
-				); ?>
-      <?php 
-	//begin wp_list_pages loop
-	if( wp_list_pages( $nav_wpflex ) ) : while ( wp_list_pages( $nav_wpflex ) ) : 
-       ?>
-      <?php wp_list_pages( $nav_wpflex ); //list items from the array above ?>
-      <?php endwhile; //end while wp_list_pages ?>
-      <?php endif; //end if wp_list_pages ?>
+      <?php
+      //wp_list_pages arguments as an array
+      $nav_wpflex = array(
+        'depth'         => 2,
+        'show_date'     => '',
+        'date_format'   => get_option( 'date_format' ),
+        'child_of'      => 0,
+        'exclude'       => '',
+        'include'       => '',
+        'title_li'      => '',
+        'echo'          => 1,
+        'authors'       => '',
+        'sort_column'   => 'menu_order',
+        'link_before'   => '',
+        'link_after'    => '',
+        'walker'  => '' 
+        );
+
+      //begin wp_list_pages loop
+      if( wp_list_pages( $nav_wpflex ) ) : while ( wp_list_pages( $nav_wpflex ) ) :
+        //list items from the array above
+        wp_list_pages( $nav_wpflex );
+      endwhile;
+      endif;
+      ?>
     </ol>
   </nav>
   <?php //endif; ?>
