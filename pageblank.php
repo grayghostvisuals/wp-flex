b9e3186dbe0673a0189c45bad934c91c3f28cb91
GrayGhostVisuals <grayghost@grayghostvisuals.com>
2013-02-13 15:26:14 -0500

    Included loop for pageblank.php. Thanks for the eagle eye Keith Kohner ;)

--- pageblank.php
+++ pageblank.php
@@ -1,14 +1,24 @@
 <?php
 /*
  * Just A Blank Template to do what you wish with :) Cheers!
- * Template Name: Page Blank
+ * Template Name: PageBlank
  */
 ?>
 
 <?php get_header(); ?>
 
-    <section class="clearfix" id="content" role="main">
-        <?php the_content(); ?>
-    </section>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <section id="content" class="<?php the_title(); ?> clearfix" role="main">
+            <header>
+                <h1><?php the_title(); ?></h1>
+            </header>
+            <?php the_content(); ?>
+            <?php wp_link_pages( array( 'before' => '<div>' . 'Pages &rarr;', 'after' => '</div>' ) ); ?>
+        </section>
+    <?php endwhile; //end while have_posts ?>
+
+    <?php else : ?>
+        <p><?php echo ( 'sorry, this page does not exist' ); ?></p>
+    <?php endif; //end if have_posts ?>
 
 <?php get_footer(); ?>
