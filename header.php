e50ddb9b5f4a4eea9a1c91ee9e71c3481234cc64
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-06-26 23:12:40 -0400

    Removes IE7 Doc Head Conditional Comments

--- header.php
+++ header.php
@@ -1,6 +1,4 @@
 <!doctype html>
-<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
 <!--[if IE 8]><html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
 <!--[if gt IE 8]><!--><html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <head>
