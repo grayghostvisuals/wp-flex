4956bc073700b2c21b08988297384a5c07007f48
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-09-09 09:15:45 -0400

    Profile Module Conditions and Cleansing

--- inc/author_meta.php
+++ inc/author_meta.php
@@ -1,36 +1,44 @@
 <?php
 	// Place this snippet within your WordPress Loop to display the
-	// post author's bio description and gravatar.
+	// post author's meta data and gravatar.
 	//
 	// http://codex.wordpress.org/Function_Reference/get_avatar
 	// http://codex.wordpress.org/Function_Reference/get_the_author_meta
 	// http://codex.wordpress.org/Function_Reference/wp_get_current_user
 	//
 	// @since 1.0.7
-	// The the_author_meta call displays the
-	// desired meta data for a post author. If this tag is
-	// used within a WP loop, the user ID value needs
-	// not be specified, and the displayed data is
-	// that of the current post author.
+	// If this tag is used within a WP loop, the user ID value needs
+	// not be specified, and the displayed data is that of the current
+	// post author.
+	//
 	//
 	// Use the following line within your post loop to pull in
-	// this author meta profile snippet.
+	// this author meta profile module:
 	//
 	// get_template_part( 'inc/author_meta' );
 ?>
 
+<?php if ( get_the_author_meta('ID') ) : ?>
 <div class="profile">
-	<!-- Avatar -->
-	<div class="profile-avatar">
-		<?php echo get_avatar( get_the_author_meta('ID'), 128 ); // 128 is your avatar size. Change as you see fit. I like 180 ?>
-	</div>
+  <div class="profile__avatar">
+    <?php echo get_avatar( get_the_author_meta('ID'), 180 ); ?>
+  </div>
 
-	<!-- Author Meta -->
-	<div class="profile-meta">
-		<h3 class="profile-name"><?php the_author(); ?></h3>
+  <div class="profile__meta">
+    <h3 class="profile-name"><?php the_author(); ?></h3>
 
-		<div class="profile-descr">
-			<?php the_author_meta('description'); ?>
-		</div>
-	</div>
+    <?php if ( get_the_author_meta('description') ) : ?>
+    <div class="profile-desc">
+      <?php the_author_meta('description'); ?>
+    </div>
+    <?php endif; ?>
+
+    <?php if ( get_the_author_meta('user_url') ) : ?>
+    <span class="profile-url">
+      <?php the_author_meta('user_url'); ?>
+    </span>
+    <?php endif; ?>
+
+  </div>
 </div>
+<?php endif; ?>
