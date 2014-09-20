437eae1ad656cf863fa50439b34c2be1d0526312
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-09-20 11:28:49 -0400

    Pagination Wording

--- category.php
+++ category.php
@@ -23,7 +23,7 @@
 		<p><?php echo ( 'Holy smokes! This is totally crazy. No posts match anything even remotely close to that in our database. Sorry Mon Frere, try again' ); ?></p>
 	<?php endif; ?>
 
-	<p><?php posts_nav_link( '&#8734;', '&larr; Go Forward In Time', 'Go Back In Time &rarr;' ); ?></p>
+	<p><?php posts_nav_link( '&#8734;', '&laquo; Previous Entries', 'Next Entries &raquo;' ); ?></p>
 </main>
 
 <aside id="sidebar" role="complementary">
