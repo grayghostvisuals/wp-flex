9c9cf8d37c1fbe8034f4db11bca39cfc2ee82e72
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-09-20 10:51:44 -0400

    Improved Title, Search Results, Pagination and Conditonal Checks

--- search.php
+++ search.php
@@ -1,41 +1,49 @@
+<?php
+	global $wp_query;
+	$big = 999999999;
+?>
+
 <?php get_header(); ?>
 
 <main id="content" class="clearfix" role="main">
 	<section>
 		<header>
-			<h1>Search Results</h1>
+			<h1 class="page-title"><?php printf( __( 'Search Results: %s', '' ), '<span class="page-title__query-term">' . get_search_query() . '</span>' ); ?></h1>
 		</header>
 
-		<ul>
+		<?php if (  have_posts() ) : ?>
+		<ul class="search-results">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+			<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 			<?php endwhile; ?>
-
-			<?php else : ?>
-				<li><p><?php echo ( 'Sorry, your search term(s) did not find any matches within our database. Please do try again won\'t you pretty please ?' ); ?></p></li>
-			<?php endif; ?>
 		</ul>
+		<?php endif; ?>
 
-		<div>
+		<?php else : ?>
+		<p><?php echo ( 'Sorry, your search term(s) did not find any matches within our database. Please do try again won\'t you pretty please ?' ); ?></p>
+		<?php endif; ?>
+
+		<?php if (  $wp_query->max_num_pages > 1 ) : ?>
+		<div class="pagination">
 			<?php
-				global $wp_query;
-				$big = 999999999;
 				echo paginate_links( array(
-				'base'         => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
-				'format'       => '?paged=%#%',
-				'total'        => $wp_query->max_num_pages,
-				'current'      => max( 1, get_query_var( 'paged' ) ),
-				'show_all'     => False,
-				'end_size'     => 1,
-				'mid_size'     => 2,
-				'prev_next'    => True,
-				'prev_text'    => __('<!--[if lte IE 8]><span id="iebtn-prev">&laquo; Previous</span><![endif]--><!--[if gt IE 8]><!--><button>&laquo; Previous</button><!--<![endif]-->'),
-				'next_text'    => __('<!--[if lte IE 8]><span id="iebtn-nxt">Next &raquo;</span><![endif]--><!--[if gt IE 8]><!--><button>Next &raquo;</button><!--<![endif]-->'),
-				'type'         => 'plain',
-				'add_args'     => False,
-				'add_fragment' => '' ) );
+					'base'         => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+					'format'       => '?paged=%#%',
+					'total'        => $wp_query->max_num_pages,
+					'current'      => max( 1, get_query_var( 'paged' ) ),
+					'show_all'     => False,
+					'end_size'     => 1,
+					'mid_size'     => 2,
+					'prev_next'    => True,
+					'prev_text'    => __('<span id="prev">&laquo; Previous</span>'),
+					'next_text'    => __('<span id="nxt">Next &raquo;</span>'),
+					'type'         => 'plain',
+					'add_args'     => False,
+					'add_fragment' => ''
+				));
 			?>
 		</div>
+	<?php endif; ?>
 	</section>
 </main>
 
