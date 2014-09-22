a05c38920e5566c0639ca99246f9bc757aedcae2
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-09-22 13:20:51 -0400

    Improves Listing Markup and Conditional Check for Display

--- inc/taxonomy.php
+++ inc/taxonomy.php
@@ -1,29 +1,25 @@
-<ul class="entry-taxonomies">
-	<?php
+<?php
 	$posttags = get_the_tags();
-	if ($posttags) :
-	?>
-	<li>
-		<?php echo ( 'Tagged:' ); ?>
-		<ul class="entry-tags-list">
-			<li class="entry-tags">
-				<?php the_tags( '</li><li>' ); ?>
-			</li>
+	$postcats = get_the_category();
+?>
+<?php if ($posttags || $postcats) : ?>
+<ul class="taxonomy">
+	<?php if ($posttags) : ?>
+	<li><?php echo ( 'Tagged:' ); ?>
+		<ul class="taxonomy__tagls taxonomy__ls">
+			<?php the_tags( '</li><li class="taxonomy__tags">' ); ?>
 		</ul>
 	</li>
 	<?php endif ?>
 
-	<?php
-	$postcats = get_the_category();
-	if ($postcats) :
-	?>
-	<li>
-		<?php echo ( 'Filed under:' ); ?>
-		<ul class="entry-categories-list">
-			<li class="entry-categories">
+	<?php if ($postcats) : ?>
+	<li><?php echo ( 'Filed under:' ); ?>
+		<ul class="taxonomy__catls taxonomy__ls">
+			<li class="taxonomy__cats">
 				<?php the_category( '</li><li>,' ) ?>
 			</li>
 		</ul>
 	</li>
 	<?php endif ?>
 </ul>
+<?php endif ?>
